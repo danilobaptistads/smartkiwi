dd0b5f6550a5a5b147341469c3c84b2308b45b7b
Danilo Baptista <damilobaptistads@gmail.com>
2025-11-13 10:28:03 -0300

    adicionada a função de setar prioridade e identificar fila prioritária

--- Services/QueueBuilder.cs
+++ Services/QueueBuilder.cs
@@ -3,15 +3,23 @@ namespace SmartKiwi.Services;
 using SmartKiwi.Models;
 public class QueueBuilder
 {
-    private List<Queue> QueueList; 
+    private List<Queue> QueueList;
     public QueueBuilder(List<Queue> queueList)
     {
         QueueList = queueList;
     }
-    public bool Build(string queueName, int queuePriority)
-    {  try
+    public bool Build(string queueName, bool isPriorityQueue = false)
+    {
+        try
         {
-            var newQueue = new Queue(queueName, queuePriority);
+            var newQueue = new Queue(queueName);
+            if (isPriorityQueue == true)
+            {
+                newQueue.IsPriorityQueue();
+                QueueList.Add(newQueue);
+
+            }
+            
             QueueList.Add(newQueue);
             return true;
         }
@@ -21,4 +29,33 @@ public class QueueBuilder
             return false;
         }
     }
+
+    public void SearchQueuePriority()
+    {
+        var i = 0;
+        Queue aux;
+        foreach (var queue in QueueList)
+        {
+            if (queue.PriorityQueue && i != 0)
+            {
+                aux = QueueList[0];
+                QueueList[0] = QueueList[i];
+                QueueList[i] = aux;
+                break;
+
+            }
+        }
+    }
+    public void SetPriorities()
+    {
+        var basePriority = QueueList.Count;
+
+        foreach(var queue in QueueList)
+        {
+            queue.SetPriority(basePriority);
+            basePriority--;
+        }
+    }
 }
+
+
