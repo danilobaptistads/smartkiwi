72cc79abf80ac634e6b6e170afe249b516e5c9cb
Danilo Baptista <damilobaptistads@gmail.com>
2026-02-24 11:26:55 -0300

    test: teste de adição de fila ao lista da QueueEngine

---
+++ SmartKiwiTest/TestQueueEngine.cs
@@ -0,0 +1,17 @@
+﻿namespace SmartKiwiTest;
+using SmartKiwiApp.Models;
+
+public class TestQueueEngine
+{
+    [Fact]
+    public void Deve_Adicionar_1_Nova_Fila_A_Lista_DE_FIlas()
+    {
+        var newQueue = new ClientQueue("TEste");
+        var queueEngine = new QueueEngine();
+        queueEngine.AddQueue(newQueue);
+        var lenghtQueueLista = queueEngine.QueueListLength();
+        Assert.Equal(1,lenghtQueue);
+    
+    }
+}
+
