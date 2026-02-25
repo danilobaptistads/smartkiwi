600c83a6f6e41c9a111cc5e16afc5fbe19170a7b
Danilo Baptista <damilobaptistads@gmail.com>
2026-02-25 10:58:32 -0300

    Test: Seguir para a proximafila se atual zerar

--- SmartKiwiTest/TestQueueEngine.cs
+++ SmartKiwiTest/TestQueueEngine.cs
@@ -13,5 +13,21 @@ public class TestQueueEngine
         Assert.Equal(1,lenghtQueueList);
     
     }
+    
+    [Fact]
+    public void Deve_Chamar_Fila_B_Se_Fila_A_Esvaziar()
+    {
+        var newQueueA = new ClientQueue("A");
+        var newQueueB = new ClientQueue("B");
+        newQueueB.Enqueue(new Client("Client_B",1));
+        var queueEngine = new QueueEngine();
+        queueEngine.AddQueue(newQueueA);
+        queueEngine.AddQueue(newQueueB);
+
+        var clientCalLed = queueEngine.ProcessQueue();
+        
+        Assert.Equal("Client_B",clientCalLed.Name);
+    
+    }
 }
 
