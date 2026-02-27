95be5c8be3f68d9e234c2c8d36c5903a6c44a261
Danilo Baptista <damilobaptistads@gmail.com>
2026-02-27 12:04:37 -0300

    Refactor: reescrita para atender os teste

--- SmartKiwiApp/QueueEgine.cs
+++ SmartKiwiApp/QueueEgine.cs
@@ -5,36 +5,91 @@ public class QueueEngine
 List<ClientQueue> queueList;
 bool isMajorQueue;
 int index;
-int nextIndex;
-int previosIndex;
+int callsCount;
+int sumPriotitys;
 ClientQueue currentQueue;
 public QueueEngine()
     {
         queueList = new List<ClientQueue>();
         isMajorQueue = true;
         index = 0;
-        nextIndex = index + 1;
-        previosIndex = index - 1;
-        
+        callsCount = 0;
+
+
     }
     public void AddQueue(ClientQueue newQueue)
     {
         queueList.Add(newQueue);
+        sumPriotitys = SumOfPrioritys();
+        
     }
     public Client ProcessQueue()
     {
-        if(queueList[index].Length() == 0)
+        currentQueue = queueList[index];
+        if(callsCount >= sumPriotitys)
         {
-            index = nextIndex;
+            resetCurrentPrioritys();
+            isMajorQueue = true;
+        }
+
+        if(currentQueue.Length() == 0 || currentQueue.currentPriority == 0)
+        {
+            index = GetNextIndex();
+            currentQueue = queueList[index];
+            
         }
         
+        var Client = currentQueue.Dequeue();
+        currentQueue.currentPriority--;
+        callsCount++;
 
-        return queueList[index].Dequeue();
+        if(isMajorQueue)
+        {
+            index = GetNextIndex();
+            currentQueue = queueList[index];
+            isMajorQueue = false;
+        }
+        else
+        {
+            index = GetPreviusIndex();
+            currentQueue = queueList[index];
+            isMajorQueue = true;
+        }
+
+        return Client;
+
+        
     }
-
-    public int QueueListLength()
+    internal int GetNextIndex()
     {
-        return queueList.Count();
+        if(index < (queueList.Count - 1))
+        {
+            index++;
+        }
+        return index;
     }
-
+    internal int GetPreviusIndex()
+    {
+        if(index > 0)
+        {
+            index--;
+        }
+        return index;
+    }
+    internal int SumOfPrioritys()
+    {
+        var sum = 0;
+        foreach(ClientQueue queue in queueList)
+        {
+            sum += queue.Priority;
+        }
+        return sum;
+    }
+    internal void resetCurrentPrioritys()
+    {
+        foreach(ClientQueue queue in queueList)
+        {
+            queue.currentPriority = queue.Priority;
+        }
+    }
 }
