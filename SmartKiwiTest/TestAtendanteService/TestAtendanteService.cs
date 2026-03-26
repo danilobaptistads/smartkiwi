aa02599f48478a30c024df3bbc26b79e06003ce8
Danilo Baptista <damilobaptistads@gmail.com>
2026-03-26 17:46:07 -0300

    fix: correção nomes dosatributos das classes call e atendante.

--- SmartKiwiTest/TestAtendanteService/TestAtendanteService.cs
+++ SmartKiwiTest/TestAtendanteService/TestAtendanteService.cs
@@ -1,34 +1,36 @@
-using System.Reflection;
+namespace SmartKiwiTest;
+using SmartKiwiApp.Models;
 
 public class TesteAtendanteService
 {
     private readonly QueueEngine queueEngine;
-    private readonly AtendenteService atendanteService;
+    private readonly AtendanteService atendanteService;
     private readonly ClientQueue queueA;
     private readonly ClientQueue queueB;
     private readonly ClientQueue queueC;
     private int maxWaiteTimeMinutes;
+    private List<ClientQueue> queueList;
     public TesteAtendanteService()
     {
         queueA = new ClientQueue("A");
         queueA.SetPriority(3);
-        queueEngine.AddQueue(queueA);
+        queueList = new(){queueA};
         maxWaiteTimeMinutes = 10;
-        queueEngine = new QueueEngine(maxWaiteTimeMinutes);
-        atendanteService = new AtendenteService(queueEngine);
+        queueEngine = new QueueEngine(maxWaiteTimeMinutes,queueList);
+        atendanteService = new AtendanteService(queueEngine);
         queueA.Enqueue(new Client("A_1", 214));
     }
 
     [Fact]
     public void Deve_Retornar_Chamada_Com_Nome_Ticket_E_Atendente_Corretos()
     {
-        var atendante = new Atendante("Atendente",5);
+        var atendante = new Atendante("Atendente","5");
         
         var newCall= atendanteService.ProcessNextCall(atendante);
         
-        Assert.Equal("A_1",newCall.name);
-        Assert.Equal(214,newCall.tickt);
-        Assert.Equal(atendante,newCall.atendante);
+        Assert.Equal("A_1",newCall.Client.Name);
+        Assert.Equal(214,newCall.Client.WaiteTicket);
+        Assert.Equal(atendante,newCall.Atendante);
         
     }
 }
