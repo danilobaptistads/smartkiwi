a5e0f8c10b387e98db6b7feb77e7ccb9c46273fe
Danilo Baptista <damilobaptistads@gmail.com>
2026-03-26 10:32:48 -0300

    Refactor: refatoração Testes de Timeoute  para QueueEngine receber queuelist por injeção

--- SmartKiwiTest/TestQueueEngine/TimeoutTests.cs
+++ SmartKiwiTest/TestQueueEngine/TimeoutTests.cs
@@ -2,19 +2,22 @@ namespace SmartKiwiTest;
 using SmartKiwiApp.Models;
 public class TimeoutTests
 {
-    private readonly QueueEngine queueEngine;
+    private readonly int maxWaiteTime;
     private readonly ClientQueue queueA;
     private readonly ClientQueue queueB;
     private readonly ClientQueue queueC;
+    private  QueueEngine queueEngine;
+    private  List<ClientQueue> queueList;
     public TimeoutTests()
     {
-        queueEngine = new QueueEngine(10);
-        queueA = new ClientQueue("A");
+         queueA = new ClientQueue("A");
         queueB = new ClientQueue("B");
         queueC = new ClientQueue("C");
         queueA.SetPriority(3);
         queueB.SetPriority(2);
         queueC.SetPriority(1);
+        queueList = new();
+        maxWaiteTime = 10;
     }
     [Theory]
     [InlineData(true,false,false,"A_1")]
@@ -22,9 +25,10 @@ public class TimeoutTests
     [InlineData(false,false,true,"C_1")]
     public void Deve_Chamar_Fila_Em_TimeOut(bool aInTimeout, bool bInTimeout,bool cInTimeout, string expectedQueue)
     {
-        queueEngine.AddQueue(queueA);
-        queueEngine.AddQueue(queueB);
-        queueEngine.AddQueue(queueC);
+        queueList.Add(queueA);
+        queueList.Add(queueB);
+        queueList.Add(queueC);
+        queueEngine = new QueueEngine(maxWaiteTime,queueList);
         queueEngine.InicializeLastcallTime();
         queueA.Enqueue(new Client("A_1", 1));
         queueB.Enqueue(new Client("B_1", 1));
@@ -59,9 +63,10 @@ public class TimeoutTests
     [InlineData(false,false,true, "C_1")]
     public void Deve_Chamar_Primera_Com_CLiente_Quanto_Todas_TimeOut(bool aHasClients, bool bHasClients,bool cHasClients, string expectedQueue)
     {
-        queueEngine.AddQueue(queueA);
-        queueEngine.AddQueue(queueB);
-        queueEngine.AddQueue(queueC);
+        queueList.Add(queueA);
+        queueList.Add(queueB);
+        queueList.Add(queueC);
+        queueEngine = new QueueEngine(maxWaiteTime,queueList);
         queueEngine.InicializeLastcallTime();
         var timeLapsedTenMinutes = DateTime.Now.AddMinutes(-11);
         queueA.lastCallTime = timeLapsedTenMinutes;
