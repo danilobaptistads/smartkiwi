3a60b6a0387decfb4bb065b379bb6b8fd70c25e6
Danilo Baptista <damilobaptistads@gmail.com>
2026-04-10 10:24:36 -0300

    Chore: AtendanteService movido para pasta Service e Criado Arquivo PasswordHashService

---
+++ SmartKiwiApp/Services/AtendanteService.cs
@@ -0,0 +1,25 @@
+using SmartKiwiApp.Models;
+namespace SmartKiwiApp.Services;
+public class AtendanteService
+{
+    private QueueEngine QueueEngine { get; set; }
+    public AtendanteService(QueueEngine queueEngine)
+    {
+        QueueEngine = queueEngine;
+    }
+
+    public Call? ProcessNextCall(Atendante atendante)
+    {
+        var clientCalled = QueueEngine.ProcessClient();
+        if(clientCalled != null)
+        {   var clientName = clientCalled.Name;
+            var atendanteName = atendante.Name;
+            var ticketWindowNumber = atendante.TicketWindow;
+            var call =  new Call(clientName, atendanteName, ticketWindowNumber);
+            return call;
+        }
+        return null;
+        
+    }
+
+}
