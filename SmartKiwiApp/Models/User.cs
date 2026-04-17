226ea6f612273fdd2cb5e9c8c6fbc0076bb4b6ca
Danilo Baptista <damilobaptistads@gmail.com>
2026-04-17 09:41:15 -0300

    Feat: Metodos de update e validação da Calsse User

--- SmartKiwiApp/Models/User.cs
+++ SmartKiwiApp/Models/User.cs
@@ -1,46 +1,62 @@
+using SmartKiwiApp.Services;
 namespace SmartKiwiApp.Models;
+using System.Text.RegularExpressions;
+
 public class User
 {
+    
+    private string _name;
+    private string _email;
+    private string _password;
     public Guid Id { get; private set; }
-    public string Name { get; private set; }
-    public string Email { get; private set; }
-    public string Password { get; private set; }
+    public string Name 
+    { 
+        get => _name; 
+        private set
+        {
+            ValidateName(value);
+            _name = value;
+        } 
+    }
+    public string Email 
+    { 
+        get => _email; 
+        private set
+        {
+            ValidateEmail(value);
+            _email =  value;
+        }
+    }
+    private string Password 
+    { 
+        get => _password;
+        set
+        {
+            ValidatePasswordFormatBase64(value);
+            _password = value;
+        } 
+    }
 
     protected User() { }
     public User(string name, string email, string password)
     {
-        Id =  Guid.NewGuid();
+        Id = Guid.NewGuid();
         Name = name;
         Email = email;
         Password = password;
     }
+
     public void UpdateEmail(string newEmail)
     {
-        if(newEmail != null)
-        {
-            Email = newEmail;   
-        }
+
+
+        Email = newEmail;
     }
     public void UpdateName(string newName)
     {
-        if(newName != null)
-        {
-            Name = newName;   
-        }
+
+        Name = newName;
     }
-    public void ChangePassword(string newPassword, string informedPassword, IPasswordHashService hasher)
-    {
-        var isValidPassword = hasher.VerifyPassword(informedPassword, Password);
-        if (!isValidPassword)
-        {
-            throw new Exception("Não foi possivel realizar a alteração");
-        }
-    
-        Password =hasher.HashPassword(newPassword);
-
-
-    }
-
     public bool ValidatePassword(string informedPassword, IPasswordHashService hasher)
     {
         var isValidPassword = hasher.VerifyPassword(informedPassword, Password);
@@ -48,8 +64,46 @@ public class User
         {
             return false;
         }
-    
+
         return true;
+
+    }
+    private void ValidateName(string name)
+    {
+        if (string.IsNullOrEmpty(name))
+            throw new ArgumentException("Nome obrigatório.");
+    }
+    private void ValidateEmail(string email)
+    {
+        if (string.IsNullOrWhiteSpace(email))
+            throw new ArgumentException("Email obrigatório.");
+
+        var pattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+
+        if (!Regex.IsMatch(email, pattern))
+            throw new ArgumentException("Email inválido.");
+    }
+    private void ValidatePasswordFormatBase64(string value)
+    {
+        if (string.IsNullOrEmpty(value) )
+        {
+            throw new ArgumentException("Senha não pode ser vazia.");
+      
+        }
+        if (!Convert.TryFromBase64String(value, new byte[value.Length], out _))
+        {
+              throw new ArgumentException("Fomato de senha inválido.");
+        }
+    }
+    public void ChangePassword(string newPassword, string informedPassword, IPasswordHashService hasher)
+    {
+        var isValidPassword = hasher.VerifyPassword(informedPassword, Password);
+        if (!isValidPassword)
+        {
+            throw new ArgumentException("Não foi possivel alterar a senha");
+        }
+
+        Password = hasher.HashPassword(newPassword);
 
     }
 }
