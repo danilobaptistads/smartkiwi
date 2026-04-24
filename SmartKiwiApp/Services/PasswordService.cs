2878763ca8699b4eec9b7a8eaf45178bcdb44d18
Danilo Baptista <damilobaptistads@gmail.com>
2026-04-24 11:59:10 -0300

    Feat: classe PasswordService para tratar novas senhas e seus testes

---
+++ SmartKiwiApp/Services/PasswordService.cs
@@ -0,0 +1,41 @@
+using System.Text.RegularExpressions;
+
+namespace SmartKiwiApp.Services;
+
+public class PasswordService
+{
+    public string ProcssesHashNewPassword(string rawPassword, IPasswordHashService hashService)
+    {
+       if(ValidatePasswordFormat(rawPassword))
+       {
+            return hashService.HashPassword(rawPassword);
+       }
+
+       throw new ArgumentException("Formato invalido");
+    }
+
+    public bool ValidatePasswordFormat(string rawPassword)
+    {
+        if (string.IsNullOrEmpty(rawPassword) )
+        {
+            return false;
+        }
+        if (rawPassword.Length < 8)
+        {
+            return false;
+        }
+        
+        var hasSpeciasChar = Regex.IsMatch(rawPassword, "[^a-zA-Z0-9]");
+        if (!hasSpeciasChar)
+        {
+            return false;
+        }
+        var hasUpercase = Regex.IsMatch(rawPassword, "[A-Z]");
+        if (!hasUpercase)
+        {
+            return false;
+        }
+        return true;
+    }
+    
+}
