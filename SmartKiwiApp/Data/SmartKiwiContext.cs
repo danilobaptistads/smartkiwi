04c7a7574bb45f4074728f5c6cf95ac0d481a5e8
Danilo Baptista <damilobaptistads@gmail.com>
2026-07-15 11:55:06 -0300

    refactor: SmartKiwiContext extendido para IdentityDbContext, para impementar o Identity

--- SmartKiwiApp/Data/SmartKiwiContext.cs
+++ SmartKiwiApp/Data/SmartKiwiContext.cs
@@ -1,11 +1,12 @@
-using Microsoft.EntityFrameworkCore;
 using SmartKiwiApp.Models;
+using Microsoft.EntityFrameworkCore;
+using  Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace SmartKiwiApp.Data;
-public class SmartKiwiContext : DbContext
-{
+public class SmartKiwiContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
+    {
     public DbSet<Call> Calls { get; set; }  
-    public DbSet<User> Users { get; set; }
     public DbSet<Client> Clients { get; set; }
     public DbSet<ClientQueue> ClientQueues { get; set; }
     public DbSet<Workstation> Workstations { get; set; }
@@ -13,4 +14,9 @@ public class SmartKiwiContext : DbContext
     {
     }
     
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+    }
 }
