515712ac464312496e4af096a258bc1b652300c9
Danilo Baptista <damilobaptistads@gmail.com>
2026-07-17 12:08:57 -0300

    chore: aplicação de camelcase nas propriedades

--- SmartKiwiApp/Dto/UserDtos.cs
+++ SmartKiwiApp/Dto/UserDtos.cs
@@ -1,8 +1,8 @@
 using SmartKiwiApp.Models;
 namespace SmartKiwiApp.Dto;
-public record CreateUserRequest (string name, string email, string rawPassword, User.Role role );
-public record UpdateNameRequest (string Id, string newName);
-public record UpdateEmailRequest (string Id, string informedPassword,string newEmail);
-public record UpdatePasswordRequest (string Id, string newPassword, string informedPassword);
- public record RemoveUserRequest(string Id,string informedPassword);
- public record LoginRequest(string informedEmail, string informedPassword);
+public record CreateUserRequest (string Name, string Email, string RawPassword, User.Role role );
+public record UpdateNameRequest (string Id, string NewName);
+public record UpdateEmailRequest (string Id, string InformedPassword,string NewEmail);
+public record UpdatePasswordRequest (string Id, string NewPassword, string InformedPassword);
+ public record RemoveUserRequest(string Id,string InformedPassword);
+ public record LoginRequest(string InformedEmail, string InformedPassword);
