d5ba41641767cf29623bb461521285db6b512e69
Danilo Baptista <damilobaptistads@gmail.com>
2026-07-17 11:07:13 -0300

    feat: adição das DTOs,  RemoveUserRequest e LoginRequest e adequação dos nomes

--- SmartKiwiApp/Dto/UserDtos.cs
+++ SmartKiwiApp/Dto/UserDtos.cs
@@ -1,8 +1,8 @@
 using SmartKiwiApp.Models;
 namespace SmartKiwiApp.Dto;
-public record CreateUserRequestDto (string name, string email, string rawPassword, User.Role role );
-public record UpdateUserNameDto (string currentUserId, string newName);
-public record UpdateUserEmailDto (string currentUserId, string informedPassword,string newEmail);
-public record UpdateUserPasswordDto (string currentUserId, string newPassword, string informedPassword);
-
-
+public record CreateUserRequest (string name, string email, string rawPassword, User.Role role );
+public record UpdateNameRequest (string Id, string newName);
+public record UpdateEmailRequest (string Id, string informedPassword,string newEmail);
+public record UpdatePasswordRequest (string Id, string newPassword, string informedPassword);
+ public record RemoveUserRequest(string Id,string informedPassword);
+ public record LoginRequest(string informedEmail, string informedPassword);
